cefc5abc886fb21eadacfff1f4753b35d2bf877d
Sabrinavf <contactsabrinafernandes@gmail.com>
2023-11-19 20:16:26 -0300

    Adicionar arquivos de projeto.

---
+++ condicional_Simples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Condiconal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double n1, n2, nota;
+
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            nota= n1+ n2;  
+            Console.WriteLine("NOTA FINAL = "+nota.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (nota <=60)
+            {
+                Console.WriteLine("REPROVADO");
+
+            }
+            else 
+            {
+                Console.WriteLine("APROVADO");
+            }
+        }
+    }
+}
